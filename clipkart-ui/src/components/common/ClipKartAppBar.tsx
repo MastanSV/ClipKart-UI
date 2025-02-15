cb31c8cf8840bcd7a8101db4c5b89e390a24c2d5
Vali S, Mastan <mastanvali157@gmail.com>
2025-02-15 23:00:54 +0530

    Added search field in the app bar

--- clipkart-ui/src/components/common/ClipKartAppBar.tsx
+++ clipkart-ui/src/components/common/ClipKartAppBar.tsx
@@ -1,18 +1,83 @@
-import { AppBar, IconButton, Typography, Toolbar, Button } from "@mui/material"
-import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar, IconButton, Typography, Toolbar, Button } from '@mui/material';
+import { styled, alpha } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  marginRight: 100,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
 
-function ClipKartAppBar()
-{
-    return <AppBar  position='static'>
-    <Toolbar>
-      <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{mr:2}} >
-        <MenuIcon></MenuIcon>
-      </IconButton>
-      <Typography variant='h6' component='div' sx={{flexGrow:1}}></Typography>
-      <Button color='inherit'>Login</Button>
-    </Toolbar>
-  </AppBar>
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+function ClipKartAppBar() {
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon></MenuIcon>
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}
+        ></Typography>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon></SearchIcon>
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default ClipKartAppBar;
